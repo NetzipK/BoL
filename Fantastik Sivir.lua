7de22ccc9a63da046d77dae37f1fb72cf41d0e75
BoLFantastik <necipaga@gmail.com>
2014-06-10 20:37:38 +0300

    Update Fantastik Sivir.lua

--- Fantastik Sivir.lua
+++ Fantastik Sivir.lua
@@ -9,26 +9,26 @@ end
 local sversion = "0.2"
 local AUTOUPDATE = true
 local UPDATE_HOST = "raw.github.com"
-local UPDATE_PATH = "/BoLFantastik/BoL/master/Fantastik Sivir.lua".."?rand="..math.random(1,10000)
-local UPDATE_FILE_PATH = SCRIPT_PATH.."Fantastik Sivir.lua"
+local UPDATE_PATH = "/BoLFantastik/BoL/master/Fantastik%20Sivir.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Fantastik%20Sivir.lua"
 local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
-function _AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>SOW:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>Fantastik Sivir:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-	local ServerData = GetWebResult(UPDATE_HOST, "/BoLFantastik/BoL/master/version/Fantastik Sivir.version")
+	local ServerData = GetWebResult(UPDATE_HOST, "/BoLFantastik/BoL/master/version/Fantastik%20Sivir.version")
 	if ServerData then
 		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
 		if ServerVersion then
 			if tonumber(sversion) < ServerVersion then
-				_AutoupdaterMsg("New version available"..ServerVersion)
-				_AutoupdaterMsg("Updating, please don't press F9")
-				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+				AutoupdaterMsg("New version available"..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..sversion.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
 			else
-				_AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
 			end
 		end
 	else
-		_AutoupdaterMsg("Error downloading version info")
+		AutoupdaterMsg("Error downloading version info")
 	end
 end
 
