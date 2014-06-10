780a68c56940f25848ad8e431266a317a193b97e
BoLFantastik <necipaga@gmail.com>
2014-06-10 20:10:54 +0300

    Update Fantastik Sivir.lua

--- Fantastik Sivir.lua
+++ Fantastik Sivir.lua
@@ -1,15 +1,35 @@
 if myHero.charName ~= "Sivir" then return end
 
 require "SOW"
-require "VPrediction"
+if VIP_USER then
+	require "VPrediction"
+end
 
 --[[		Auto Update		]]
+local sversion = "0.2"
 local AUTOUPDATE = true
-local SCRIPT_NAME = "Fantastik Sivir"
-local sversion = "0.01"
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/BoLFantastik/BoL/master/Fantastik Sivir.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH.."Fantastik Sivir.lua"
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
+function _AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>SOW:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
 if AUTOUPDATE then
-SourceUpdater(SCRIPT_NAME, version, "raw.github.com", "/BoLFantastik/BoL/master/"..SCRIPT_NAME..".lua", SCRIPT_PATH .. GetCurrentEnv().FILE_NAME, "/BoLFantastik/BoL/master/version/"..SCRIPT_NAME..".version"):CheckUpdate()
+	local ServerData = GetWebResult(UPDATE_HOST, "/BoLFantastik/BoL/master/version/Fantastik Sivir.version")
+	if ServerData then
+		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+		if ServerVersion then
+			if tonumber(version) < ServerVersion then
+				_AutoupdaterMsg("New version available"..ServerVersion)
+				_AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () _AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+			else
+				_AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		_AutoupdaterMsg("Error downloading version info")
+	end
 end
 
 --[[		Code		]]
@@ -17,10 +37,9 @@ local sauthor = "Fantastik"
 local QREADY, WREADY, EREADY, RREADY = false
 local Qrange, Qwidth, Qspeed, Qdelay = 1075, 85, 1350, 0.250
 local Rrange = 1000
-local ignite = nil -- by default and before the check it will be always nil
+local ignite = nil
 local iDmg = 0
 local target = nil
--- Vars for TargetSelector --
 local ts
 local ts = TargetSelector(TARGET_LESS_CAST_PRIORITY, Qrange, DAMAGE_PHYSICAL, true)
 
@@ -32,13 +51,13 @@ function OnLoad()
 	PrintChat("<font color=\"##58D3F7\"><b>Evadeee</b> found! You can use Evadeee integration!")
 	_G.Evadeee_Enabled = true
 	end
-  	IgniteCheck() -- don't flood your general functions with to much code - make it clean by adding own functions
+  	IgniteCheck()
 	SLoadLib()
 end
 
 
 function OnTick()
-  	ts:update() -- updates your targetselector on every tick
+  	ts:update()
   	target = ts.target
   	Checks()
 	
@@ -47,25 +66,25 @@ function OnTick()
 		if SivMenu.Extra.Ignite then AutoIgnite(target) end
 	end
 	
-   if SivMenu.Combo.combokey then
+   if SivMenu.combokey then
 		Combo()
    end
-   if SivMenu.Poke.pokekey then
+   if SivMenu.pokekey then
 		Poke()
    end
    if SivMenu.Extra.Evade then
        EvadeeeHelper()
    end
 end
+
 function Checks()
-  -- Checks for your cooldowns on every tick as it is implemented in OnTick()
 	QREADY = (myHero:CanUseSpell(_Q) == READY)
 	WREADY = (myHero:CanUseSpell(_W) == READY)
 	EREADY = (myHero:CanUseSpell(_E) == READY)
 	RREADY = (myHero:CanUseSpell(_R) == READY)
   	IREADY = (ignite ~= nil and myHero:CanUseSpell(ignite) == READY)
 end
--- Checks which SummonerSpell is ignite (implemented to OnLoad) - u only need to do this once
+
 function IgniteCheck()
 	if myHero:GetSpellData(SUMMONER_1).name:find("SummonerDot") then
 			ignite = SUMMONER_1
@@ -73,6 +92,7 @@ function IgniteCheck()
 			ignite = SUMMONER_2
 	end
 end
+
 function OnDraw()
 	if SivMenu.Drawing.DrawAA then
 	 SOWi:DrawAARange()
@@ -93,17 +113,20 @@ end
 
 function SMenu()
 	SivMenu = scriptConfig("Fantastik Sivir", "Sivir")
+	SivMenu:addParam("combokey", "Combo key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+	SivMenu:addParam("pokekey", "Poke key", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
 	SivMenu:addParam("Version", "Version", SCRIPT_PARAM_INFO, sversion)
 	SivMenu:addParam("Author", "Author", SCRIPT_PARAM_INFO, sauthor)
 	SivMenu:addTS(ts)
 	SivMenu:addSubMenu("Combo", "Combo")
-	SivMenu.Combo:addParam("combokey", "Combo key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
+--	SivMenu.Combo:addParam("combokey", "Combo key", SCRIPT_PARAM_ONKEYDOWN, false, 32)
 	SivMenu.Combo:addParam("comboQ", "Use Q", SCRIPT_PARAM_ONOFF, true)
 	SivMenu.Combo:addParam("comboW", "Use W", SCRIPT_PARAM_ONOFF, true)
 	SivMenu.Combo:addParam("comboR", "Use R", SCRIPT_PARAM_ONOFF, true)
+	SivMenu.Combo:addParam("minEnemiesR", "Min. no. of enemies for R", SCRIPT_PARAM_SLICE, 1, 1, 5, 0)
 	
 	SivMenu:addSubMenu("Poke", "Poke")
-	SivMenu.Poke:addParam("pokekey", "Poke key", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
+--	SivMenu.Poke:addParam("pokekey", "Poke key", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("C"))
 	SivMenu.Poke:addParam("pokeQ", "Use Q", SCRIPT_PARAM_ONOFF, true)	
 	
 	SivMenu:addSubMenu("Drawing", "Drawing")
@@ -120,8 +143,8 @@ function SMenu()
 	if _G.Evadeee_Loaded then
 		SivMenu.Extra:addParam("Evade", "Use Evadeee Integration", SCRIPT_PARAM_ONOFF, true)
 	end
-	SivMenu.Combo:permaShow("combokey")
-	SivMenu.Poke:permaShow("pokekey")
+	SivMenu:permaShow("combokey")
+	SivMenu:permaShow("pokekey")
 end
 
 function KS(Target)
@@ -132,14 +155,7 @@ function KS(Target)
 		end
 	end
 end
---[[
-function AutoIgnite(target)
-	if target.health <= iDmg then
-		if iReady then CastSpell(ignite, target) end
-	end
-end]] -- <- your function 
--- first of all make functions general (enemy/unit not your direct target as it can bug out sometimes if you call your ts.target = target)
--- next thing you should check the range for your spell (this time 600 for igniterange)
+  
 function AutoIgnite(enemy)
   	iDmg = ((IREADY and getDmg("IGNITE", enemy, myHero)) or 0) 
 	if enemy.health <= iDmg and GetDistance(enemy) <= 600 and ignite ~= nil
@@ -157,16 +173,20 @@ end
 function Combo()
   if ValidTarget(target) then
 		if QREADY and SivMenu.Combo.comboQ then
-		local CastPos = VP:GetLineCastPosition(target, Qdelay, Qwidth, Qrange, Qspeed, myHero, true)
-		if GetDistance(target) <= Qrange and QREADY then
-			CastSpell(_Q, CastPos.x, CastPos.z)
-		end
+        if VIP_USER then
+			local CastPos = VP:GetLineCastPosition(target, Qdelay, Qwidth, Qrange, Qspeed, myHero, true)
+			if GetDistance(target) <= Qrange and QREADY then
+				CastSpell(_Q, CastPos.x, CastPos.z)
+			end
+        else
+        CastSpell(_Q, target.x, target.z)
+        end
 	end
 	if WREADY and SivMenu.Combo.comboW and GetDistance(target) <= 600 then
 		CastSpell(_W)
 	end
 	if RREADY and SivMenu.Combo.comboR and GetDistance(target) <= 600 then
-		CastSpell(_R)
+		CastR()
 	end
   end
 end
@@ -175,10 +195,20 @@ function Poke()
   if ValidTarget(target) then
 		if SivMenu.Poke.pokeQ and QREADY
     	then
-		local CastPos = VP:GetLineCastPosition(target, Qdelay, Qwidth, Qrange, Qspeed, myHero, true)
-		if GetDistance(target) <= Qrange and QREADY then
-			CastSpell(_Q, CastPos.x, CastPos.z)
-		end
+        if VIP_USER then
+		 local CastPos = VP:GetLineCastPosition(target, Qdelay, Qwidth, Qrange, Qspeed, myHero, true)
+		 if GetDistance(target) <= Qrange and QREADY then
+		  CastSpell(_Q, CastPos.x, CastPos.z)
+		 end
+       else
+        CastSpell(_Q, target.x, target.z)
+       end
 	end
    end
+end
+
+function CastR()
+	if SivMenu.Combo.minEnemiesR <= CountEnemyHeroInRange(600) then
+		CastSpell(_R)
+	end
 end
